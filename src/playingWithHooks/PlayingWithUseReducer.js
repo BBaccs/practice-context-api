76333f49a88b01dfbd1951e087112eb29ada8519
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-01 19:34:49 -0400

    Update PlayingWithUseReducer.js

--- src/playingWithHooks/PlayingWithUseReducer.js
+++ src/playingWithHooks/PlayingWithUseReducer.js
@@ -1,24 +1,26 @@
 import React, { useReducer } from 'react';
 
-function reducer(state, action){
-    if (action.type === "INCREMENT") {
-        return { count: state.count + 1 };
+function reducer(state, action) {
+    switch (action.type) {
+        case "INCREMENT":
+        return { count: state.count + action.amount };
+        case "DECREMENT":
+        return { count: state.count - action.amount };
+        case "RESET":
+        return { count: 0 };
     }
-    if (action.type === "DECREMENT") {
-        return { count: state.count - 1 };
-        
-    }
-
 }
 
 export default function PlayingWithUseReducer() {
-const [ state, dispatch ] = useReducer(reducer, { count: 0})
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
     return (
         <div>
             <br />
             <p>Playing with useReducer: {state.count}</p>
-            <button onClick={() => dispatch({ type: "INCREMENT" })}>ADD +1</button>
-            <button onClick={() => dispatch({ type: "DECREMENT" })}>Subtract -1</button>
+            <button onClick={() => dispatch({ type: "INCREMENT", amount: 1 })}>ADD +1</button>
+            <button onClick={() => dispatch({ type: "DECREMENT", amount: 1 })}>Subtract -1</button>
+            <button onClick={() => dispatch({ type: "INCREMENT", amount: 5 })}>ADD +5</button>
+            <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
         </div>
     )
 }
